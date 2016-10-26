25f266f35b169fbf75c9cebabafe49a00985d90b
Manikandan Sudhaman <manikandan.sudhaman@turner.com>
2016-10-26 14:57:45 -0400

    Simple shell script to replace all the hex codes in the file to ascii values

---
+++ dehex_file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+########################################################################
+#
+# Replace all the hex codes in the file to ascii values
+# Example: \x3c  -->  <
+#          \x3e  -->  >
+#
+########################################################################
+
+input_file=$1
+path="$(dirname $input_file)"
+
+sed '
+s/\\x3c/</g
+s/\\x3e/>/g
+s/\\x3d/=/g
+s/\\x22/"/g
+s/\\x26/&/g
+s/\\\\/\\/g
+s/\\t/	/g
+s/\\n/ \
+/g' < $input_file | sed "s/\\x27/'/g" > $path/temp.txt
+
+mv $path/temp.txt $input_file
